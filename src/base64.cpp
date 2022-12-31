8645472e8eb0e5a4aa9711d8e7e544336f1fc0f6
Pushkar Raj <px86@protonmail.com>
2022-12-31 14:12:58 +0530

    (#1) Fix - add support for `--wrap` option.

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,8 +1,10 @@
 #include "base64.hpp"
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <cstring> // memcpy
+#include <sstream>
 
 // default implementations of feed_data
 void pr::Base64::feed_data(std::uint8_t data) { m_data.push_back(data); }
@@ -20,7 +22,7 @@ void pr::Base64::feed_data(std::vector<uint8_t>&& data)
     m_data = std::move(data);
 }
 
-auto pr::Base64Encoder::encode() -> std::string
+auto pr::Base64Encoder::encode(size_t wrap_after) -> std::string
 {
     auto m_data = data();
     std::uint8_t* d = m_data.data();
@@ -29,38 +31,57 @@ auto pr::Base64Encoder::encode() -> std::string
     std::size_t blocks = size / 3UL; // number of 3 (8-bit) bytes blocks
     std::size_t remainder = size % 3UL;
 
-    auto encoded_str = std::string {}; // FIXME: reserve sufficient space beforehand
+    auto strm = std::ostringstream {};
     auto byte_pos = size_t { 0UL };
+    auto buff = std::array<char, 4> { 0 };
+
+    auto wrap_pos = size_t { 0UL };
+    auto insert_chars = [&]() {
+        // Insert chars into stream
+        for (char c : buff) {
+            strm << c;
+            if (wrap_after > 0 && ++wrap_pos == wrap_after) {
+                strm << '\n';
+                wrap_pos = 0UL;
+            }
+        }
+    };
 
     for (auto block = 0UL; block < blocks; ++block) {
-        encoded_str.push_back(nth_of_alphabet(d[byte_pos] >> 2));
-        encoded_str.push_back(
-            nth_of_alphabet(((d[byte_pos] & 0x03U) << 4) | (d[byte_pos + 1] >> 4)));
-        encoded_str.push_back(
-            nth_of_alphabet(((d[byte_pos + 1] & 0x0fU) << 2) | (d[byte_pos + 2] >> 6)));
-        encoded_str.push_back(nth_of_alphabet(d[byte_pos + 2] & 0x3f));
+        buff[0] = nth_of_alphabet(d[byte_pos] >> 2);
 
+        buff[1] = nth_of_alphabet(
+            (d[byte_pos] & 0x03U) << 4 | d[byte_pos + 1] >> 4);
+
+        buff[2] = nth_of_alphabet(
+            (d[byte_pos + 1] & 0x0fU) << 2 | d[byte_pos + 2] >> 6);
+
+        buff[3] = nth_of_alphabet(d[byte_pos + 2] & 0x3f);
+
+        insert_chars();
         byte_pos += 3;
     }
 
-    if (remainder == 0) {
-        return encoded_str;
-    }
-    // do not increment byte_pos now
-    encoded_str.push_back(nth_of_alphabet(d[byte_pos] >> 2));
+    if (remainder != 0) {
+        buff[0] = nth_of_alphabet(d[byte_pos] >> 2);
+        if (remainder == 2) {
+            buff[1] = nth_of_alphabet(
+                ((d[byte_pos] & 0x03U) << 4) | (d[byte_pos + 1] >> 4));
+            buff[2] = (nth_of_alphabet((d[byte_pos + 1] & 0x0fU) << 2));
+        } else {
+            buff[1] = (nth_of_alphabet((d[byte_pos] & 0x03U) << 4));
+            buff[2] = '=';
+        }
 
-    if (remainder == 2) {
-        encoded_str.push_back(
-            nth_of_alphabet(
-                ((d[byte_pos] & 0x03U) << 4) | (d[byte_pos + 1] >> 4)));
-        encoded_str.push_back(nth_of_alphabet((d[byte_pos + 1] & 0x0fU) << 2));
-    } else {
-        encoded_str.push_back(nth_of_alphabet((d[byte_pos] & 0x03U) << 4));
-        encoded_str.push_back('=');
+        buff[3] = '=';
+        insert_chars();
     }
 
-    encoded_str.push_back('=');
-    return encoded_str;
+    auto enc_str = strm.str();
+    if (enc_str.back() == '\n') {
+        enc_str.pop_back();
+    }
+    return enc_str;
 }
 
 auto pr::Base64Decoder::decode() -> std::vector<uint8_t>
@@ -142,7 +163,7 @@ auto pr::read_entire_file(const char* filepath) -> std::optional<std::vector<uin
     return buff;
 }
 
-auto pr::encode_file(const char* filepath) -> std::optional<std::string>
+auto pr::encode_file(const char* filepath, size_t wrap_after) -> std::optional<std::string>
 {
     auto opt = pr::read_entire_file(filepath);
     if (!opt.has_value()) {
@@ -152,7 +173,7 @@ auto pr::encode_file(const char* filepath) -> std::optional<std::string>
     auto encoder = pr::Base64Encoder {};
     encoder.feed_data(std::move(buff));
 
-    return encoder.encode();
+    return encoder.encode(wrap_after);
 }
 
 auto pr::decode_file(const char* filepath) -> std::optional<std::vector<uint8_t>>
